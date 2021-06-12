42014df4d71b2a5910ec14628f89e42d7b27ba90
Anna <annnabondar@gmail.com>
2021-06-12 12:43:32 -0400

    tables

--- db/index.js
+++ db/index.js
@@ -1,13 +1,71 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const { Client } = require("pg");
+const DB_NAME = "change-this-name";
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
 
+async function createLink({ url, comments, tags = [] }) {
+  try {
+    const {
+      rows: [link],
+    } = await client.query(
+      `
+    INSERT INTO links(url, comments)
+    VALUES($1, $2)
+    RETURNING *;
+    `,
+      [url, comments]
+    );
+
+    const tagList = await createTags(tags);
+    return await addTagsToLink(link.id, tagList);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function createTags(tagList) {
+  if (tagList.length === 0) {
+    return;
+  }
+
+  const insertTags = tagList.map((_, index) => `$${index + 1}`).join("), (");
+
+  const selectTags = taglist.map((_, index) => `$${index + 1}`).join(", ");
+}
+
+try {
+  await client.query(
+    `
+INSERT INTO tags(tagname)
+VALUES(${insertTags})
+ON CONFLICT (tagname) DO NOTHING;
+`,
+    tagList
+  );
+} catch (error) {
+  console.log(error);
+}
+
+async function createLinkTag(linkId, tagId) {
+  try {
+    await client.query(
+      `
+    INSERT INTO link_tags("linkId", "tagId")
+    VALUES ($1, $2)
+    ON CONFLICT ("linkId", "tagId") DO NOTHING;`,
+      [linkId, tagId]
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // export
 module.exports = {
   client,
+  createLinkTag,
   // db methods
-}
+};
--- db/init_db.js
+++ db/init_db.js
@@ -1,8 +1,8 @@
 // code to build and initialize DB goes here
 const {
-  client
-  // other db methods 
-} = require('./index');
+  client,
+  // other db methods
+} = require("./index");
 
 async function buildTables() {
   try {
@@ -10,8 +10,35 @@ async function buildTables() {
 
     // drop tables in correct order
 
-    // build tables in correct order
+    await client.query(`
+    DROP TABLE IF EXISTS link_tags;
+    DROP TABLE IF EXISTS tags;
+    DROP TABLE IF EXISTS links;
+    `);
 
+    // build tables in correct order
+    await client.query(`
+    CREATE TABLE links(
+    id SERIAL PRIMARY KEY,
+    url VARCHAR(255) UNIQUE NOT NULL,
+    clicks INTEGER DEFAULT 0,
+    date TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    comments TEXT NOT NULL
+);`);
+
+    await client.query(`
+    CREATE TABLE tags(
+      id SERIAL PRIMARY KEY,
+      tagname VARCHAR(255) UNIQUE NOT NULL
+    );`);
+
+    await client.query(`
+    CREATE TABLE link_tags(
+      "linkId" INTEGER REFERENCES links(id),
+      "tagId" INTEGER REFERENCES tags(id),
+      UNIQUE ("linkId", "tagId")
+    );`);
+    console.log("Finished building tables");
   } catch (error) {
     throw error;
   }
