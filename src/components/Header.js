a12113ef5690bc673b7a28aabfef069c71053567
Jason <60307987+jlcolli79@users.noreply.github.com>
2021-06-19 22:40:55 -0400

    added header functionality and starter styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { sortAllLinksByPopularity } from "../api";
+import { getAllLinks, sortAllLinksByPopularity } from "../api";
 
 const Header = ({ allLinks, setAllLinks }) => {
   const sortLinksHandler = async () => {
@@ -7,19 +7,46 @@ const Header = ({ allLinks, setAllLinks }) => {
     setAllLinks([]);
     setAllLinks(sorted);
   };
+
+  const allLinksHandler = async () => {
+    const allLinks = await getAllLinks();
+    setAllLinks([]);
+    setAllLinks(allLinks);
+  };
+
   return (
-    <header>
-      <h2>LINKERATOR</h2>
-      <h4>your bookmark app</h4>
-      <button
-        onClick={(event) => {
-          event.preventDefault();
-          sortLinksHandler();
-        }}
-      >
-        Show most visited first
-      </button>
-    </header>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item is-size-2 has-text-weight-bold has-background-info has-text-white">
+          Linkerator!
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start is-size-4">
+          <a onClick={allLinksHandler} className="navbar-item">
+            All Links
+          </a>
+          <a onClick={sortLinksHandler} className="navbar-item">
+            Sorted by Popularity
+          </a>
+        </div>
+
+        <div className="navbar-end"></div>
+      </div>
+    </nav>
   );
 };
 
--- src/components/SingleLink.js
+++ src/components/SingleLink.js
@@ -4,7 +4,38 @@ const SingleLink = ({ singleLink }) => {
   const { url, comments, clicks, date, tags } = singleLink;
 
   return (
-    <div className="single-link">
+    <div className="card my-2">
+      <div className="card-content">
+        <div className="content">
+          <p>{url}</p>
+          <p>
+            Clicked {clicks} since {date}
+          </p>
+          <p>Comments: {comments}</p>
+        </div>
+      </div>
+      <footer className="card-footer is-align-self-auto">
+        <p className="card-footer-item">
+          Tags:
+          <br />
+          {tags &&
+            tags.map((tag) => {
+              const { id, tagname } = tag;
+              return (
+                <a className="tag has-background-info has-text-white mx-1" key={id}>
+                  {tagname}
+                </a>
+              );
+            })}
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default SingleLink;
+
+/* <div className="single-link">
       <p>{url}</p>
       <p>
         Clicked {clicks} since {date}
@@ -18,8 +49,4 @@ const SingleLink = ({ singleLink }) => {
             return <p key={id}>{tagname}</p>;
           })}
       </div>
-    </div>
-  );
-};
-
-export default SingleLink;
+    </div> */
